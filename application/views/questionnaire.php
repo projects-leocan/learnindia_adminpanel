9c647fa1bd820d3b5d5d875dc4af9cd661675351
ubuntu1 <miralgaywala@gmail.com>
2023-06-13 11:10:55 +0530

    title alignment

--- application/views/questionnaire.php
+++ application/views/questionnaire.php
@@ -78,7 +78,7 @@
             <div class="container-fluid">
                 <div class="row mb-2">
                     <div class="col-sm-10">
-                        <h1 class="m-0 mx-2">Add Questionnaire</h1>
+                        <h1 class="m-0 mx-3">Add Questionnaire</h1>
                     </div>
                     <div class="col-sm-2">
                         <ol class="breadcrumb float-sm-right">
